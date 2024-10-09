c46a53dd9d48c0913d4684569d95d55f1f42698f
Rahul Dandavolu <rahuldan@umich.edu>
2024-10-08 22:41:30 -0400

    TESTS

--- Card_tests.cpp
+++ Card_tests.cpp
@@ -19,22 +19,43 @@ TEST(test_default_ctor) {
 
 TEST(test_get_suit_two) {
     Card c(JACK, SPADES);
+    Card d(ACE, DIAMONDS);
+    ASSERT_EQUAL(DIAMONDS, d.get_suit(DIAMONDS));
     ASSERT_EQUAL(CLUBS, c.get_suit(CLUBS));
 }
 
 TEST(test_is_face_or_ace) {
-    Card c(ACE, DIAMONDS);
+    Card a(ACE, DIAMONDS);
+    Card b(JACK, SPADES);
+    Card c(KING, SPADES);
+    Card d(QUEEN, CLUBS);
+    Card e(NINE, HEARTS);
+    Card f(TEN, DIAMONDS);
+    ASSERT_EQUAL(true, a.is_face_or_ace());
+    ASSERT_EQUAL(true, b.is_face_or_ace());
     ASSERT_EQUAL(true, c.is_face_or_ace());
+    ASSERT_EQUAL(true, d.is_face_or_ace());
+    ASSERT_EQUAL(false, e.is_face_or_ace());
+    ASSERT_EQUAL(false, f.is_face_or_ace());
 }
 
 TEST(test_is_right_bower) {
     Card c(JACK, CLUBS);
+    Card d(JACK, SPADES);
+    Card e(KING, DIAMONDS);
     ASSERT_EQUAL(true, c.is_right_bower(CLUBS));
+    ASSERT_EQUAL(false, d.is_right_bower(CLUBS));
+    ASSERT_EQUAL(false, e.is_right_bower(DIAMONDS));
 }
 
 TEST(test_is_left_bower) {
     Card c(JACK, CLUBS);
+    Card d(JACK, SPADES);
+    Card e(KING, DIAMONDS);
     ASSERT_EQUAL(true, c.is_left_bower(SPADES));
+    ASSERT_EQUAL(false, d.is_left_bower(SPADES));
+    ASSERT_EQUAL(false, e.is_left_bower(HEARTS));
+    ASSERT_EQUAL(false, e.is_left_bower(DIAMONDS));
 }
 
 TEST(test_is_trump) {
@@ -70,7 +91,9 @@ TEST(test_led_card_less) {
     Card c(QUEEN, SPADES);
     Card d(ACE, DIAMONDS);
     Card e(JACK, HEARTS);
+    Card f(TEN, CLUBS);
     Card led(KING, SPADES);
+    Card led2(NINE, HEARTS);
 
     ASSERT_EQUAL(false, Card_less(c, d, led, CLUBS));
     ASSERT_EQUAL(false, Card_less(c, d, led, SPADES));
@@ -80,6 +103,10 @@ TEST(test_led_card_less) {
     ASSERT_EQUAL(true, Card_less(e, d, led, SPADES));
     ASSERT_EQUAL(false, Card_less(e, d, led, DIAMONDS));
     ASSERT_EQUAL(false, Card_less(e, d, led, HEARTS));
+    ASSERT_EQUAL(true, Card_less(c, f, led2, CLUBS));
+    ASSERT_EQUAL(false, Card_less(d, f, led2, HEARTS));
+    ASSERT_EQUAL(false, Card_less(e, f, led2, DIAMONDS));
+    ASSERT_EQUAL(false, Card_less(d, f, led2, SPADES));
 }
 // Add more test cases here
 
